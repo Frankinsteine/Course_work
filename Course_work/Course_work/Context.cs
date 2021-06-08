20df238e9b9068018723d59e32966d7d16a8af21
Frankinsteine <61928670+Frankinsteine@users.noreply.github.com>
2021-06-08 21:24:45 +0400

    Add files via upload

---
+++ Course_work/Course_work/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_work
+{
+    class Context : DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+        public DbSet<ProductName> ProductName { get; set; }
+        public DbSet<Manufacturer> Manufacturer { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<OrderProducts> OrderProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ProductShopDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Course_work/Course_work/Manufacturer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_work
+{
+    class Manufacturer
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
